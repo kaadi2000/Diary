408f46a3ee223f253116db6a4b6d079ef1b18d05
Kumar Aditya <aditya.g.2005001@gmail.com>
2021-06-09 13:14:31 +0530

    Update editor.py

--- assets/editor.py
+++ assets/editor.py
@@ -1,38 +1,27 @@
 import os
 from tkinter import *
-def open():
+from datetime import datetime
+from tkinter import messagebox
+
+def run():
     root=Tk("New Entry")
     text=Text(root)
     text.grid()
     def close():
-        exit()
+        root.destroy()
     def saveas():
-        global text
         t = text.get("1.0", "end-1c")
-        savelocation=os.chdir(os.environ['USERPROFILE'])
-        file1=open(savelocation, "w+")
-        file1.write(t)
+        now = datetime.now()
+        filename = now.strftime("%d%m%Y%H%M%S") + ".data"
+        with open(filename, 'w') as file1:
+            file1.write(t)
         file1.close()
+        messagebox.showinfo("+ New Entry", "File Save Successful")
+        root.destroy()
     button=Button(root, text="Save", command=saveas)
     button.grid()
     button1 = Button(root, text = "Cancel", command= close)
     button1.grid()
-    def FontHelvetica():
-        global text
-        text.config(font="Helvetica")
-    def FontCourier():
-        global text
-        text.config(font="Courier")
-    font=Menubutton(root, text="Font") 
-    font.grid() 
-    font.menu=Menu(font, tearoff=0) 
-    font["menu"]=font.menu
-    Helvetica=IntVar() 
-    arial=IntVar() 
-    times=IntVar() 
-    Courier=IntVar()
-    font.menu.add_checkbutton(label="Courier", variable=Courier, 
-    command=FontCourier)
-    font.menu.add_checkbutton(label="Helvetica", variable=Helvetica,
-    command=FontHelvetica) 
+
+    root.title("+ New Entry")
     root.mainloop()
